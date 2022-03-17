0f543b94fbef5f1ec8501755d02a7d6dfd699641
YBeb <younes.bouchi@gmail.com>
2022-03-17 06:58:04 +0100

    maj

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,5 @@
 const url = 'http://127.0.0.1:3000/api/products';
-const descriptionKanap = document.getElementById('description')
-
+const kanapImg = document.querySelector('.item__img')
 var urlParams = new URLSearchParams(location.search);
 let kanapIndex = urlParams.get('id');
 
@@ -12,15 +11,32 @@ fetch(url)
 
     .then(function (data) {
         // descriptionKanap.innerText = data[kanapIndex].description
-        getKanapByID(data)
-        console.log('testda')
+        const kanap = getKanapByID(data)
+        let descriptionKanap = document.getElementById('description');
+        let priceKanap = document.getElementById('price');
+        let bigKanapImg = document.createElement('img');
+        bigKanapImg.src = kanap.imageUrl;
+        kanapImg.appendChild(bigKanapImg)
+        descriptionKanap.innerText = kanap.description;
+        priceKanap.innerText = kanap.price
+        
     })
     .catch(function () {
 
     })
 
 var getKanapByID = function (kanapData) {
-    let i = -1
+    kanapImg.innerHTML=""
+    for (let i = 0; i < kanapData.length; i++) {
+        if (kanapData[i]._id == kanapIndex) {
+            return kanapData[i]
+        }
+
+        
+    }
+
+
+    /*let i = -1
     console.log('test')
     do {
         i++
@@ -29,9 +45,9 @@ var getKanapByID = function (kanapData) {
         console.log(currentID != kanapIndex)
 
     }
-    while (i < kanapData.length)
+    while (i < kanapData.length);
     console.log('test')
-    //console.log(kanapData[i].name)
+    //console.log(kanapData[i].name)*/
 }
 
 
