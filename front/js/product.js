989670186fdaaa1acee32345b184fdf8322b4ddb
YBeb <younes.bouchi@gmail.com>
2022-03-28 12:43:55 +0200

    maj code

--- front/js/product.js
+++ front/js/product.js
@@ -2,6 +2,7 @@ const kanapImg = document.querySelector('.item__img')
 var urlParams = new URLSearchParams(location.search);
 let kanapIndex = urlParams.get('id');
 const url = `http://127.0.1:3000/api/products/${kanapIndex}`;
+var items = []
 fetch(url)
     .then(function (response) {
 
@@ -27,10 +28,67 @@ fetch(url)
         }
         document.getElementById('colors').innerHTML = kanapColor;
 
+
+        const colorValue = () => {
+            const inputValueColor = document.getElementById("colors").value;
+            
+          }
+        const itemQuantity = () =>{ const inputValueQuantity=document.getElementById("quantity").value;
+       
+    }
+        document.getElementById('addToCart').addEventListener('click', ajoutPanier);
+
+
+        function ajoutPanier() {
+            var productCart = { id: data._id, color: colorValue, quantity: itemQuantity };
+            //Si la quantité est inférieur a zéro
+            if (quantity <= 0) {
+                alert('La quantité dois être strictement supérieur a zéro');
+            }
+            else {
+                //Si la couleur est vide 
+                if (colorValue == "") {
+                    alert('La couleur doit être choisis')
+                }
+                //Dans le cas contraire si tout fonctionne , récupérer du localStorage le produit dont on a besoin
+                else {
+                    var jsonString = localStorage.getItem("productCart");
+                    var retrievedObject = JSON.parse(jsonString);
+                    // Si le tableau n'est pas vide , le vidé;
+                    if (!retrievedObject || retrievedObject == 0) {
+                        retrievedObject = [];
+                    }
+                    // On créer un tableau vide
+                    let producToSave = [];
+                    for (let produit in retrievedObject) {
+                        //si le produit
+                        if ((produit.id == productCart.id) && (produit.color == productCart.color)) {
+                            productCart.quantity += product.quantity;
+                            producToSave.push(productCart);
+                        }
+                        else {
+                            producToSave.push(producToSave);
+                            producToSave.push(produit);
+
+                        }
+                    }
+                    localStorage.setItem("productCart", JSON.stringify(producToSave));
+                }
+
+            }
+        }
+
+
+
     })
     .catch(function (error) {
         console.log(error);
         console.log('error API');
         alert('Le serveur est indisponible pour le moment');
     })
+
+
+
+
+
 
