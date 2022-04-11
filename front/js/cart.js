69be1d9071cf31a1fde1ec3853784ce16fd18524
YBeb <younes.bouchi@gmail.com>
2022-04-12 00:15:37 +0200

    maj

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,61 +1,57 @@
 //On récupére le localstorage et on le parse
-let addProduct = JSON.parse(localStorage.getItem("productCart"));
-const itemCart = document.getElementById("cart__items")
-
-const url = 'http://127.0.0.1:3000/api/products';
+const itemCart = document.getElementById("cart__items");
+var addProduct = JSON.parse(localStorage.getItem("productCart"));
 // Le role du fetch est de créer une promesse au niveau de l'API afin de recuperer les informations
-fetch(url)
-    // Configurer une fonction pour gérer la reponse renvoyer par la promesse 
-    .then(function (response) {
+
+if (!addProduct || addProduct==[] || addProduct.lengty < 1) {
+  itemCart.innerHTML = "<h3>Le Panier est vide<h3>"
+}
+else {
+  itemCart.innerHTML = "";
+  addProduct.forEach(productI => {
+    let productId = productI.id;
+    fetch(`http://localhost:3000/api/products/${productId}`)
+      .then(function (response) {
         return response.json();
-    })
-    //Configurer une fonction qui vas traiter la reponse en format JSON de l'API
-    .then(function (data) {
+      })
+      .then(function (data) {
+        let product = {
+          'color': productI.color,
+          'quantity': parseInt(productI.quantity),
+          'name': data.name,
+          'price': data.price,
+          'imgUrl': data.imageUrl,
+          'id': productId,
+        };
+        itemCart.innerHTML += ` <article class="cart__item" data-id="${product._id}" data-color="${product.color}">
+  <div class="cart__item__img">
+    <img src="${product.imgUrl}" alt="Photographie d'un canapé">
+  </div>
+  <div class="cart__item__content">
+    <div class="cart__item__content__description">
+      <h2>${product.name}</h2>
+      <p>${product.color}</p>
+      <p>${product.price*product.quantity}€</p>
+    </div>
+    <div class="cart__item__content__settings">
+      <div class="cart__item__content__settings__quantity">
+        <p>Qté : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+      </div>
+      <div class="cart__item__content__settings__delete">
+        <p class="deleteItem">Supprimer</p>
+      </div>
+    </div>
+  </div>
+</article> `
+      })
+  });
 
-    })
-  
-    .catch (function (error) {
-        console.log(error)
-    
-    })
 
-    if (!addProduct) {
-      itemCart.innerHTML = "<h3>Le Panier est vide<h3>"
-  
-  }
-  else {
 
-      let tabProductCart = [];
-      for (f = 0; f < addProduct.length; f++) {
-          tabProductCart = tabProductCart + `<section id="cart__items">
-          <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-                 <div class="cart__item__img">
-                   <img src=""../images/product01.jpg" alt="Photographie d'un canapé">
-                 </div>
-                 <div class="cart__item__content">
-                   <div class="cart__item__content__description">
-                     <h2>Nom du produit</h2>
-                     <p>${addProduct[f].color}</p>
-                     <p>42,00 €</p>
-                   </div>
-                   <div class="cart__item__content__settings">
-                     <div class="cart__item__content__settings__quantity">
-                       <p>Qté : </p>
-                       <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${addProduct[f].quantity}">
-                     </div>
-                     <div class="cart__item__content__settings__delete">
-                       <p class="deleteItem">Supprimer</p>
-                     </div>
-                   </div>
-                 </div>
-               </article> 
-             </section>`
-      }
-      if (f == addProduct.length) {
-          itemCart.innerHTML = tabProductCart;
-      }
-  
-  }
+
+}
+
 
 
 
