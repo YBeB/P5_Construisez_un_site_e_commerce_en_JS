a112987fa081ca80dd118fb19cdc4e1382b431b1
YBeb <younes.bouchi@gmail.com>
2022-04-19 06:58:31 +0200

    maj

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,7 +8,7 @@ function panierDisplay() {
         cartItems.innerHTML = 'Votre panier est vide.';
         calculPrixTotal();
         //On met un addEventListener sur le bouton order qui affiche une fonction qui alerte l'utilisateur que le panier est vide donc non validable
-        document.getElementById('order').addEventListener('click', function(e) {
+        document.getElementById('order').addEventListener('click', function (e) {
             e.preventDefault();
             alert('Votre panier est vide.');
         })
@@ -21,9 +21,9 @@ function panierDisplay() {
             let productId = oneProduct.id;
             //On fait une promesse pour appelé l'API
             fetch(`http://localhost:3000/api/products/${productId}`)
-                .then(function(response) {
+                .then(function (response) {
                     return response.json();
-                }).then(function(resolve) {
+                }).then(function (resolve) {
                     //On crée une variable product et lui attribut la couleur ,l'id et la quantité contenu dans le localStorage et on complete la suite grace a l'API
                     let product = {
                         'color': oneProduct.color,
@@ -43,7 +43,7 @@ function panierDisplay() {
                     <div class="cart__item__content__description">
                         <h2>${product.name}</h2>
                         <p>${product.color}</p>
-                        <p id='productPrice_${product._id}_${product.color}'>${product.price * parseInt(product.quantity) } €</p>
+                        <p id='productPrice_${product._id}_${product.color}'>${product.price * parseInt(product.quantity)} €</p>
                     </div>
                     <div class="cart__item__content__settings">
                         <div class="cart__item__content__settings__quantity">
@@ -59,7 +59,7 @@ function panierDisplay() {
                     counter++;
                     return counter;
                     //On créé un autre then avec une fonction
-                }).then(function(counter) {
+                }).then(function (counter) {
                     //Si la variable counter (valeur numérique) est strictement  égale a la taille du tableau du localStorage
                     if (counter == productSaved.length) {
                         //alors on déclare ces 3 fonctions
@@ -73,7 +73,7 @@ function panierDisplay() {
 }
 
 const calculPrixTotal = () => {
-  //On attribut une valeur numérique de départ(donc 0) a total et qteTotal
+    //On attribut une valeur numérique de départ(donc 0) a total et qteTotal
     let total = 0;
     let qteTotal = 0;
     //On tranforme le JSON en valeur Javascript
@@ -84,7 +84,7 @@ const calculPrixTotal = () => {
         fetch(`http://localhost:3000/api/products/${productId}`)
             .then(item => item.json())
             .then(data => {
-              //On multiplie le prix et la quantité et on addtionne avec le total
+                //On multiplie le prix et la quantité et on addtionne avec le total
                 total += data.price * parseInt(oneProduct.quantity);
                 try {
                     //Ici , on capture dans le dom "productPrice" on l'addition avec productId(enJS) et on le couple avec la couleur égale au prix fois 
@@ -105,13 +105,13 @@ const calculPrixTotal = () => {
 }
 //Création d'une constante pour le changement de la quantité
 const changeQuantite = () => {
-  //On selectionne la classe itemQuantity
+    //On selectionne la classe itemQuantity
     const quantityInputs = document.querySelectorAll('.itemQuantity');
     //On execute la fonction qu'on vas crée pour chaque element du tableau avec forEach
     quantityInputs.forEach(input => {
-          //On luit attribut un event Listener avec l'attribut change en tant qu'event pour actionner le callback si un changement au niveau de l'input est fait
+        //On luit attribut un event Listener avec l'attribut change en tant qu'event pour actionner le callback si un changement au niveau de l'input est fait
         input.addEventListener('change', () => {
-          //On crée une constante avec closest a l'interieur , ca permet de selectionner l'element le plus proche
+            //On crée une constante avec closest a l'interieur , ca permet de selectionner l'element le plus proche
             const articleToRemove = input.closest('article');
             //On récuperer l'atribut data-id et data-color, avec getAttribute
             const idProductToChange = articleToRemove.getAttribute('data-id');
@@ -121,23 +121,23 @@ const changeQuantite = () => {
             productSaved.forEach(product => {
                 //Si le l'id du produit est strictement égale a l'attribut data-id et si la couleur du produit et strictement egale a l'atribut data-color
                 if (product.id === idProductToChange && product.color === colorProductToChange) {
-                  //Si l'input Value est inferieur a zero
+                    //Si l'input Value est inferieur a zero
                     if (input.value < 0) {
-                      //Alors on pousse le produit dans le tableau  mais on ne met pas a jour la quantité
+                        //Alors on pousse le produit dans le tableau  mais on ne met pas a jour la quantité
                         finalProductSave.push(product);
                         //On alerte l'utilisateur qu'il faut absolument mettre une quantité superieur a zero
                         alert("la quantite doit etre supérieure ou égale à zéro");
                         //Si l'utilisateur indique une quantité de zero alors le produit est tout simplement supprimé 
                     } else if (input.value == 0) {
-                      //On selectione l'article le plus proche avec articleToRemove et on le supprime avec le .remove dans le DOM
+                        //On selectione l'article le plus proche avec articleToRemove et on le supprime avec le .remove dans le DOM
                         articleToRemove.remove();
                     } else {
-                     //Autrement si le produit et baisser ou augmente , alors on met a jour la quantité et on le pousse dans le tableau 
+                        //Autrement si le produit et baisser ou augmente , alors on met a jour la quantité et on le pousse dans le tableau 
                         product.quantity = input.value;
                         finalProductSave.push(product);
                     }
                 } else {
-                  //Si l'id ne corresponds pas a l'id de Data-ID ni la couleur de data-color alors on pousse sans rien changé , donc identique
+                    //Si l'id ne corresponds pas a l'id de Data-ID ni la couleur de data-color alors on pousse sans rien changé , donc identique
                     finalProductSave.push(product);
                 }
                 //On met a jour les valeurs  localStorage dans le tableau crée
@@ -157,7 +157,7 @@ function removeProduct() {
         btn.addEventListener('click', () => {
             //Ici même procéder que sur le changement de la quantité , on selectionne l'élement le plus proche
             const articleToRemove = btn.closest('article');
-             //On récuperer l'atribut data-id et data-color, avec getAttribute
+            //On récuperer l'atribut data-id et data-color, avec getAttribute
             const idProductToDelete = articleToRemove.getAttribute('data-id');
             const colorProductToDelete = articleToRemove.getAttribute('data-color');
             //On tranforme le JSON en valeur Javascript
@@ -182,8 +182,69 @@ function removeProduct() {
         });
     })
 }
-//On déclare la fonction panierDisplay pour qu'elle fonctionne
+//On appel la fonction panierDisplay pour qu'elle fonctionne
 panierDisplay();
+const InfirstName = document.getElementById("firstName");
+const InlastName = document.getElementById("lastName");
+const Inaddress = document.getElementById("address");
+const Incity = document.getElementById("city");
+const InEmail = document.getElementById("email");
 
 
+function formValidator() {
+    const regexEmail = new RegExp('/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/')
+    const regexAdress = new RegExp('\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\.');
+    const regexfirstName = new RegExp('/^(?=.{1,50}$)[a-z]+(?:[_.\s][a-z]+)*$/i');
+    const regexLastName = new RegExp('/^(?=.{1,50}$)[a-z]+(?:[_.\s][a-z]+)*$/i');
+    const regexCity = new RegExp('/^(?=.{1,50}$)[a-z]+(?:[_.\s][a-z]+)*$/i')
+    const form = document.querySelector('cart__order__form');
+
+
+
+    var firstNameCheck = function (InfirstName) {
+        let errorFirstName = document.getElementById("firstNameErrorMsg")
+        if (regexfirstName.test(InfirstName.value)) {
+            errorFirstName.innerHTML = '';
+        }
+        else {
+            errorFirstName.innerHTML = 'Veuillez entrer votre prénom (uniquement en lettre)'
+
+        }
+
+    };
+
+    var lastNameCheck = function (InlastName) {
+        let errorLastName = document.getElementById("lastNameErrorMsg")
+        if (regexLastName.test(InlastName.value)) {
+            errorLastName.innerHTML = '';
+        }
+        else { errorLastName.innerHTML = 'Veuillez entrer votre nom (uniquement en lettre)' }
+    }
+
+    var adressCheck = function (Inaddress) {
+        let errorAdress = document.getElementById("addressErrorMsg");
+        if (regexAdress.test(Inaddress.value)) {
+            errorAdress.innerHTML = ''
+        }
+        else { errorAdress.innerHTML = 'Veuillez entrer votre adresse (ex:12 Rue Dupont)' }
+    }
+
+    var cityCheck = function (Incity) {
+        let errorCity = document.getElementById("cityErrorMsg");
+        if (regexCity.test(Incity.value)) {
+            errorCity.innerHTML = ''
+        }
+        else { errorCity.innerHTML = 'Veuillez entrer votre ville (ex:  Paris)' }
+    }
+
+
+    var emailCheck = function (InEmail) {
+        let errorEmail = document.getElementById("emailErrorMsg");
+        if (regexEmail.test(InEmail.value)) {
+            errorEmail.innerHTML = ''
+        }
+        else { errorEmail.innerHTML = 'Veuillez entrer votre email (ex:Jean.Dupont@live.fr)' }
+    }
+
+}
 
